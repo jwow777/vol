d83f61a7f787f85201e531a02fa7acd6ab90366a
Aleksandr <Raspertovalexandr@yandex.ru>
2021-08-15 14:25:07 +0300

    fix design header and main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,9 +13,8 @@ import {
   Popover,
   SwipeableDrawer,
 } from '@material-ui/core';
-import { Call, ChevronRight, Menu } from '@material-ui/icons';
+import { ChevronRight, Menu } from '@material-ui/icons';
 import clsx from 'clsx';
-import logo from '../../images/header/logo.svg';
 import ru from '../../images/flags/ru.png';
 import us from '../../images/flags/us.png';
 import './Header.css';
@@ -168,7 +167,7 @@ function Header({ openPopup }) {
           Оставить заявку
         </Button>
         <div className='header__lang-swipe-box' onClick={handleMenuLang}>
-          <img src={language} alt='language' className='header__lang-current-swipe-box'/>
+          <p className='header__lang-current-swipe-box'>{language}</p>
           <p className='header__lang-text'>{language === ru ? 'Русский' : 'English'}</p>
         </div>
       </div>
@@ -177,43 +176,44 @@ function Header({ openPopup }) {
   return (
     <header className='header'>
       <div className='header__container'>
-        <div className='header__logo-box'>
-          <Link to='/'>
-            <img src={logo} alt='Estraid Broker' className='header__logo'/>
-          </Link>
-          <p className='header__title'>VOL</p>
-        </div>
-        <div className='header__button-box'>
-          <a href='tel:+74954899696' className='header__phone'>
-            <Call fontSize='large'/>
-            <p className='header__phone-text'>+7 (495) 489-96-96</p>
-          </a>
-          <img src={language} alt='language' className='header__lang-current' onClick={handleMenuLang}/>
-          <Popover
-            anchorEl={anchorElLang}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'center',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-            open={openLang}
-            onClose={handleCloseLang}
-            className='header__lang-box'
-          >
-            <MenuList>
-              <MenuItem onClick={handleCloseLang} lang={ru}>
-                <img src={ru} alt='Русский' className='header__lang-image'/>
-                Русский
-              </MenuItem>
-              <MenuItem onClick={handleCloseLang} lang={us}>
-                <img src={us} alt='English' className='header__lang-image'/>
-                English
-              </MenuItem>
-            </MenuList>
-          </Popover>
+        <Link to='/' className='header__logo'>VOL</Link>
+        <div className='header__box header__box_right'>
+          <nav className='header__menu'>
+            <ul className='header__menu-list'>
+              <li className='header__menu-item'>Продукт</li>
+              <li className='header__menu-item'>Мы</li>
+              <li className='header__menu-item'>Цены</li>
+            </ul>
+          </nav>
+          <div className='header__box header__box_button'>
+            <p className='header__lang-current' onClick={handleMenuLang}>{language === ru ? 'RU' : 'EN'}</p>
+            <Popover
+              anchorEl={anchorElLang}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+              open={openLang}
+              onClose={handleCloseLang}
+              className='header__lang-box'
+            >
+              <MenuList>
+                <MenuItem onClick={handleCloseLang} lang={ru}>
+                  <img src={ru} alt='Русский' className='header__lang-image'/>
+                  Русский
+                </MenuItem>
+                <MenuItem onClick={handleCloseLang} lang={us}>
+                  <img src={us} alt='English' className='header__lang-image'/>
+                  English
+                </MenuItem>
+              </MenuList>
+            </Popover>
+            <button type="button" className='header__download'>Скачать</button>
+          </div>
           <IconButton
             className="header__menu-button-mobile"
             onClick={toggleDrawer(true)}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,69 +1,91 @@
-import React from 'react';
-import { Button, makeStyles } from '@material-ui/core';
-import computer from '../../images/main/computer.png';
-import phone from '../../images/main/phone.png';
+/* eslint-disable consistent-return */
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import PhoneInput from 'react-phone-input-2';
 import './Main.css';
+import devicesImg from '../../images/main/devices.png';
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    background: `url(${phone}) 1135px 250px no-repeat, url(${computer}) 870px 90px no-repeat`,
-    maxWidth: 1440,
-    margin: 'auto',
-    boxSizing: 'border-box',
-    padding: '80px 100px',
-    [theme.breakpoints.down('xs')]: {
-      background: `url(${phone}) 120% 60px / 50% no-repeat, url(${computer}) 20px 50px / 65% no-repeat`,
-      padding: '75% 20px 50px',
-    },
-  },
-  button: {
-    height: 50,
-    fontWeight: 500,
-    fontSize: 20,
-    lineHeight: 24,
-    borderRadius: 5,
-    backgroundColor: '#679AFD',
-    fontFamily: '"Inter", sans-serif',
-    fontStyle: 'normal',
-    letterSpacing: '-0.035em',
-    color: '#fff',
-    textTransform: 'none',
-    overflow: 'hidden',
-    '&:hover': {
-      backgroundColor: '#1565FF',
-    },
-    [theme.breakpoints.down('xs')]: {
-      minHeight: 45,
-      maxWidth: 300,
-      width: '100%',
-      fontWeight: 400,
-      fontSize: 16,
-      lineHeight: 1.2,
-    },
-  },
-}));
+function Main() {
+  const [formSuccess, setFormSuccess] = useState(false);
+  const [formState, setFormState] = useState({
+    phone: '',
+    country: {},
+  });
 
-function Main({ openPopup }) {
-  const classes = useStyles();
+  const handleChangePhone = (phone, country) => {
+    setFormState({
+      ...formState,
+      phone,
+      country: {
+        country: country.name,
+        dialCode: country.dialCode,
+      },
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formState.phone) {
+      return fetch(`${window.location.href}connector.php`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          action: 'app_form',
+          body: {
+            phone: formState.phone,
+            country: formState.country,
+            dialCode: formState.dialCode,
+          },
+        }),
+      })
+        .then((res) => {
+          if (res.ok) res.json();
+          // eslint-disable-next-line prefer-promise-reject-errors
+          return Promise.reject(`Что-то пошло не так: ${res.status}`);
+        })
+        .then(() => setFormSuccess(true))
+        .catch((err) => console.log(err));
+    }
+  };
 
   return (
     <main className='main'>
-      <div className={classes.container}>
+      <div className='main__container'>
         <div>
           <h1 className='title'>
-            Корпоративный&nbsp;
-            <span className='title text_blue'>мессенджер </span>
-            для&nbsp;эффективных команд
+            Корпоративный мессенджер для бизнеса
           </h1>
-          <p className='subtitle'>Корпоративная рабочая область, удобные упоминания, обсуждения, чаты, расшифровка аудиосообщений</p>
-          <Button
-            variant='contained'
-            color='primary'
-            className={classes.button}
-            onClick={openPopup}
-          >
-            Попробовать 14 дней бесплатно
-          </Button>
+          <p className='subtitle'>Новый уровень общения в команде</p>
+          {
+            !formSuccess
+              ? (
+                <form className='main__form' onSubmit={handleSubmit}>
+                  <PhoneInput
+                    country='ru'
+                    placeholder='7 (999) 999-99-99'
+                    value={formState.phone}
+                    onChange={handleChangePhone}
+                  />
+                  <button type='submit' className='main__submit'>
+                    Подключиться
+                    <svg width="19" height="13" viewBox="0 0 19 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd" d="M0.410156 6.65037C0.410156 6.47909 0.478195 6.31483 0.599305 6.19372C0.720415 6.07261 0.884675 6.00457 1.05595 6.00457H16.2877L12.223 1.94123C12.1018 1.81997 12.0336 1.6555 12.0336 1.48401C12.0336 1.31252 12.1018 1.14805 12.223 1.02679C12.3443 0.905527 12.5088 0.837402 12.6802 0.837402C12.8517 0.837402 13.0162 0.905527 13.1375 1.02679L18.3038 6.19314C18.364 6.25313 18.4117 6.3244 18.4442 6.40285C18.4768 6.48131 18.4935 6.56542 18.4935 6.65037C18.4935 6.73531 18.4768 6.81942 18.4442 6.89788C18.4117 6.97633 18.364 7.0476 18.3038 7.10759L13.1375 12.2739C13.0162 12.3952 12.8517 12.4633 12.6802 12.4633C12.5088 12.4633 12.3443 12.3952 12.223 12.2739C12.1018 12.1527 12.0336 11.9882 12.0336 11.8167C12.0336 11.6452 12.1018 11.4808 12.223 11.3595L16.2877 7.29616H1.05595C0.884675 7.29616 0.720415 7.22812 0.599305 7.10701C0.478195 6.9859 0.410156 6.82164 0.410156 6.65037Z" fill="white"/>
+                    </svg>
+                  </button>
+                  <p className='main__trying'>Попробуйте <span>14 дней бесплатно</span></p>
+                </form>
+              )
+              : (
+                <p className='main__thanks'>
+                  <span>Спасибо!</span> Продукт сейчас в тестовом режиме. Мы начислим 14 дней бесплатного доступа, как только опубликуем рабочую версию продукта.
+                </p>
+              )
+          }
+        </div>
+        <div className='main__box-image'>
+          <img src={devicesImg} alt='phone' className='main__image main__image_computer' />
         </div>
       </div>
     </main>
